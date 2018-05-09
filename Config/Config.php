fcc8d37f89724808c90c282c44b09cd4abf919c6
cedric85 <cedricjager@hotmail.fr>
2018-05-09 18:15:30 +0200

    données accès bdd

---
+++ Config/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+class ConfigBD {
+
+	public static function database() {
+
+	$key = [
+	'username' => 'cedri934008_1kbccj',
+	'password' => 'jfort',
+	'host' => '185.98.131.90',
+	'db_name' => 'cedri934008_1kbccj'
+	];
+
+	return $key;
+
+	}
+}
